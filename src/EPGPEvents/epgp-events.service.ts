aa4ab5e1bded8d1bf7f5632b509dcb62be943863
Brox Huang <moodfamily0@gmail.com>
2020-05-11 19:11:20 -0600

    bulk action

--- src/EPGPEvents/epgp-events.service.ts
+++ src/EPGPEvents/epgp-events.service.ts
@@ -4,10 +4,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { CreateEPGPEventDto } from './dto';
 import { EPGPEvent, LootItem } from './epgp-events.interface';
 
-const ITEM_NAME = 'item.name';
-const ITEM_RARITY = 'item.rarity';
-const ITEM_ID = 'item.itemId';
-
 @Injectable()
 export class EPGPEventsService {
   constructor(
@@ -16,44 +12,22 @@ export class EPGPEventsService {
     private lootItemModel: Model<LootItem>,
   ) {}
 
-  async create(
-    createEPGPEventDtos: CreateEPGPEventDto[],
-  ): Promise<EPGPEvent[]> {
-    return Promise.all(
-      createEPGPEventDtos.map(event => {
-        const item = {
-          ...(event.item && {
-            [ITEM_NAME]: event.item.name,
-            [ITEM_RARITY]: event.item.rarity,
-            [ITEM_ID]: event.item.itemId,
-          }),
-        };
+  async create(createEPGPEventDtos: CreateEPGPEventDto[]): Promise<boolean> {
+    const bulkUpdate = this.epgpEventModel.collection.initializeOrderedBulkOp();
 
-        return this.epgpEventModel.findOneAndUpdate(
-          {
-            $and: [
-              {
-                name: event.name,
-              },
-              { timestamp: event.timestamp },
-              { type: event.type },
-              { value: event.value },
-            ],
-          },
-          {
-            $set: {
-              name: event.name,
-              reason: event.reason,
-              value: event.value,
-              timestamp: event.timestamp,
-              eventType: event.type,
-              ...item,
-            },
-          },
-          { upsert: true, new: true },
-        );
-      }),
-    );
+    createEPGPEventDtos.forEach(event => {
+      bulkUpdate
+        .find({
+          name: event.name,
+          timestamp: event.timestamp,
+          type: event.type,
+          value: event.value,
+        })
+        .upsert()
+        .updateOne(new this.epgpEventModel(event));
+    });
+
+    return (await bulkUpdate.execute()).ok;
   }
 
   async findEvents(name: string): Promise<EPGPEvent[]> {
